3815628113108fd67092cbb23e457ce36a5c80dd
Brian-OCarroll <bocarroll36@gmail.com>
2020-02-12 19:20:20 -0800

    ported portfolio to node

---
+++ public/javascripts/index.js
@@ -0,0 +1,11 @@
+$('.nav-link, .project-btn').click(function() {
+    var anchor = $(this).attr("dest");
+    // $('.link-wrap').removeClass('visible');
+
+    // $('nav span').removeClass('active');
+    $("nav").find('[dest="'+ anchor +'"]').addClass('active');
+
+    $('html, body').animate({
+      scrollTop: $('#' + anchor).offset().top
+    }, 400);
+});
